636bcde14fbeea1d38464d104b78775bbf86e16e
K-Magara <magara@maizuru-ct.ac.jp>
2022-04-08 14:33:52 +0900

    引数処理　途中

--- Avi2Still.py
+++ Avi2Still.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import argparse
 import cv2
 import numpy as np
 
@@ -6,10 +8,10 @@ import numpy as np
 ## py Mov2Still.py foo.avi
 ## カレントディレクトリに foo.png で動画が静止画に
 
-def m_slice(path, dir, step, extension):
+def m_slice(path, dir, extension, step):
     movie = cv2.VideoCapture(path)                  # 動画の読み込み
     Fs = int(movie.get(cv2.CAP_PROP_FRAME_COUNT))   # 動画の全フレーム数を計算
-    path_head = dir + '\out_'                       # 静止画のファイル名のヘッダー
+    path_head = dir + '_'                           # 静止画のファイル名のヘッダー
     ext_index = np.arange(0, Fs, step)              # 静止画を抽出する間隔
 
     for i in range(Fs - 1):                         # フレームサイズ分のループを回す
@@ -21,28 +23,24 @@ def m_slice(path, dir, step, extension):
             # もしi番目のフレームが静止画を抽出するものであれば、ファイル名を付けて保存する
             if True in check:
                 # ファイル名は後でフォルダ内で名前でソートした時に連番になるようにする
-                if i < 10:
-                    path_out = path_head + '0000' + str(i) + extension
-                elif i < 100:
-                    path_out = path_head + '000' + str(i) + extension
-                elif i < 1000:
-                    path_out = path_head + '00' + str(i) + extension
-                elif i < 10000:
-                    path_out = path_head + '0' + str(i) + extension
-                else:
-                    path_out = path_head + str(i) + extension
+                path_out = path_head + str(i).zfill(5) + extension
+                print(path_out)
                 cv2.imwrite(path_out, frame)
-            # i番目のフレームが静止画を抽出しないものであれば、何も処理をしない
-            else:
-                pass
-        else:
-            pass
     return
 
 # 引数処理
-if len(sys.argv) < 1:
-    print('Usage: py Mov2Still 動画ファイル名')
+parser = argparse.ArgumentParser(description='動画ファイルから静止画を出力します．')
+parser.add_argument('inFile', help='分割したい動画ファイル名')
+parser.add_argument('-o', help='出力フォルダ．省略するとカレントディレクトリに出力されます．')
+parser.add_argument('-s', help='動画分割数．')
+args = parser.parse_args()
+
+print(args.inFile)
+exit()
+
+if len(sys.argv) <= 1:
+    print()
     exit()
 
-# 関数実行：引数=（ファイル名のパス、保存先のフォルダパス、ステップ数、静止画拡張子）
-m_slice(sys.argv[1], 'dir', 50, '.png')
+# 関数実行：引数=（ファイル名のパス、保存先のフォルダパス、静止画拡張子、ステップ数）
+m_slice(sys.argv[1], os.path.splitext(sys.argv[1])[0], '.png', 50)
